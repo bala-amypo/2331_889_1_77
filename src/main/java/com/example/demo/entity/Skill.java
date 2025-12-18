f9913e13ed634315a3e5be126f2c780dac1471cd
bala-amypo <bala@amypo.in>
2025-12-18 09:12:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/Skill.java
+++ src/main/java/com/example/demo/entity/Skill.java
@@ -6,5 +6,57 @@ public class Skill{
     private String skillName;
     private String category;
     private String description;
+    private Double minCompetencyScore;
+    private Boolean active;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getSkillName() {
+        return skillName;
+    }
+    public void setSkillName(String skillName) {
+        this.skillName = skillName;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Double getMinCompetencyScore() {
+        return minCompetencyScore;
+    }
+    public void setMinCompetencyScore(Double minCompetencyScore) {
+        this.minCompetencyScore = minCompetencyScore;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public Skill(Long id, String skillName, String category, String description, Double minCompetencyScore,
+            Boolean active) {
+        this.id = id;
+        this.skillName = skillName;
+        this.category = category;
+        this.description = description;
+        this.minCompetencyScore = minCompetencyScore;
+        this.active = active;
+    }
+    public Skill() {
+    }
+
+
+
 
 }
