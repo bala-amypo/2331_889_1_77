9209be7a869b20ee1a151c30c49892ecc29ed7e1
bala-amypo <bala@amypo.in>
2025-12-18 08:52:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 public class StudentProfile{
     private Long id;
+    @Column(unique=true)
     private String entrollmentId;
     private String cohort;
     private Integer yearLevel;
