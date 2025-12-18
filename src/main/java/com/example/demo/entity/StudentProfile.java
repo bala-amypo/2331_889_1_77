00f324710605d76b532036d556e7e93985204290
bala-amypo <bala@amypo.in>
2025-12-18 09:02:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentProfile.java
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -1,4 +1,4 @@
-package com.example.demo.entity;
+ppackage com.example.demo.entity;
 
 public class StudentProfile{
     private Long id;
@@ -7,4 +7,45 @@ public class StudentProfile{
     private String cohort;
     private Integer yearLevel;
     private Boolean active;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEntrollmentId() {
+        return entrollmentId;
+    }
+    public void setEntrollmentId(String entrollmentId) {
+        this.entrollmentId = entrollmentId;
+    }
+    public String getCohort() {
+        return cohort;
+    }
+    public void setCohort(String cohort) {
+        this.cohort = cohort;
+    }
+    public Integer getYearLevel() {
+        return yearLevel;
+    }
+    public void setYearLevel(Integer yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public StudentProfile(Long id, String entrollmentId, String cohort, Integer yearLevel, Boolean active) {
+        this.id = id;
+        this.entrollmentId = entrollmentId;
+        this.cohort = cohort;
+        this.yearLevel = yearLevel;
+        this.active = active;
+    }
+    public StudentProfile() {
+    }
+    
+
 }
