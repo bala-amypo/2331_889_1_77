f560f7414c545683c56fe6d2962e49cd6a30165a
bala-amypo <bala@amypo.in>
2025-12-18 09:47:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/SkillGapRecommendation.java
@@ -0,0 +1,7 @@
+package.com.example.demo.entity;
+
+public class SkillGapRecommendation{
+    private Long id;
+    private String recommendedAction;
+    private 
+}
--- src/main/java/com/example/demo/entity/SkillGapRecord.java
+++ src/main/java/com/example/demo/entity/SkillGapRecord.java
@@ -3,4 +3,49 @@ package.com.example.demo.entity;
 public class SkillGapRecord{
     private Long id;
     private Double currentScore;
+    private Double targetScore;
+    private Double gapScore;
+    private Timestamp calculatedAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Double getCurrentScore() {
+        return currentScore;
+    }
+    public void setCurrentScore(Double currentScore) {
+        this.currentScore = currentScore;
+    }
+    public Double getTargetScore() {
+        return targetScore;
+    }
+    public void setTargetScore(Double targetScore) {
+        this.targetScore = targetScore;
+    }
+    public Double getGapScore() {
+        return gapScore;
+    }
+    public void setGapScore(Double gapScore) {
+        this.gapScore = gapScore;
+    }
+    public Timestamp getCalculatedAt() {
+        return calculatedAt;
+    }
+    public void setCalculatedAt(Timestamp calculatedAt) {
+        this.calculatedAt = calculatedAt;
+    }
+    public SkillGapRecord(Long id, Double currentScore, Double targetScore, Double gapScore, Timestamp calculatedAt) {
+        this.id = id;
+        this.currentScore = currentScore;
+        this.targetScore = targetScore;
+        this.gapScore = gapScore;
+        this.calculatedAt = calculatedAt;
+    }
+    public SkillGapRecord() {
+    }
+    
+
+
 }
