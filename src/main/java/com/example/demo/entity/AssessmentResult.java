04818d2cfe894f9ca9fbf9e158bb805a275fc8e9
bala-amypo <bala@amypo.in>
2025-12-18 09:37:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/AssessmentResult.java
+++ src/main/java/com/example/demo/entity/AssessmentResult.java
@@ -5,4 +5,36 @@ public class AssessmentResult{
     private Double scoreObtained;
     private Double maxScore;
     private Timestamp assessedAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Double getScoreObtained() {
+        return scoreObtained;
+    }
+    public void setScoreObtained(Double scoreObtained) {
+        this.scoreObtained = scoreObtained;
+    }
+    public Double getMaxScore() {
+        return maxScore;
+    }
+    public void setMaxScore(Double maxScore) {
+        this.maxScore = maxScore;
+    }
+    public Timestamp getAssessedAt() {
+        return assessedAt;
+    }
+    public void setAssessedAt(Timestamp assessedAt) {
+        this.assessedAt = assessedAt;
+    }
+    public AssessmentResult(Long id, Double scoreObtained, Double maxScore, Timestamp assessedAt) {
+        this.id = id;
+        this.scoreObtained = scoreObtained;
+        this.maxScore = maxScore;
+        this.assessedAt = assessedAt;
+    }
+    public AssessmentResult() {
+    }
 }
