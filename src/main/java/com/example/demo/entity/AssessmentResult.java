7bb8b66533471bd20469d74fcf1b0cae295f7d35
bala-amypo <bala@amypo.in>
2025-12-18 09:32:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/AssessmentResult.java
+++ src/main/java/com/example/demo/entity/AssessmentResult.java
@@ -3,5 +3,6 @@ package.com.example.demo.entity;
 public class AssessmentResult{
     private Long id;
     private Double scoreObtained;
-    
+    private Double maxScore;
+    private Timestamp assessedAt;
 }
