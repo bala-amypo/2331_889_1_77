70ad381756fdfef31fe6cf7e050224dcc269a154
bala-amypo <bala@amypo.in>
2025-12-18 08:47:38 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/StudentProfile.java
@@ -0,0 +1,9 @@
+package com.example.demo.entity;
+
+public class StudentProfile{
+    private Long id;
+    private String entrollmentId;
+    private String cohort;
+    private Integer yearLevel;
+    private Boolean active;
+}
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -10,4 +10,66 @@ public class User{
     private String INSTRUCTOR;
     private String STUDENT;
     private Timestamp createdAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getADMIN() {
+        return ADMIN;
+    }
+    public void setADMIN(String aDMIN) {
+        ADMIN = aDMIN;
+    }
+    public String getINSTRUCTOR() {
+        return INSTRUCTOR;
+    }
+    public void setINSTRUCTOR(String iNSTRUCTOR) {
+        INSTRUCTOR = iNSTRUCTOR;
+    }
+    public String getSTUDENT() {
+        return STUDENT;
+    }
+    public void setSTUDENT(String sTUDENT) {
+        STUDENT = sTUDENT;
+    }
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+    public User(Long id, String fullName, String email, String password, String aDMIN, String iNSTRUCTOR,
+            String sTUDENT, Timestamp createdAt) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        ADMIN = aDMIN;
+        INSTRUCTOR = iNSTRUCTOR;
+        STUDENT = sTUDENT;
+        this.createdAt = createdAt;
+    }
+    public User() {
+    }
+    
 }
