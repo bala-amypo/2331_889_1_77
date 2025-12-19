4a96062cd1385c34f8afeeae4eb8ccd43da0e470
bala-amypo <bala@amypo.in>
2025-12-19 04:25:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/StudentProfileService.java
@@ -0,0 +1,62 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.StudentProfile;
+import com.example.demo.repository.StudentProfileRepository;
+
+@Service
+public class StudentProfileServiceImpl implements StudentProfileService {
+
+    @Autowired
+    private StudentProfileRepository repository;
+
+    @Override
+    public StudentProfile saveStudentProfile(StudentProfile profile) {
+
+        // Default active = true
+        if (profile.getActive() == null) {
+            profile.setActive(true);
+        }
+
+        return repository.save(profile);
+    }
+
+    @Override
+    public StudentProfile getStudentProfileById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public StudentProfile getByEntrollmentId(String entrollmentId) {
+        return repository.findByEntrollmentId(entrollmentId);
+    }
+
+    @Override
+    public List<StudentProfile> getAllStudentProfiles() {
+        return repository.findAll();
+    }
+
+    @Override
+    public StudentProfile updateStudentProfile(Long id, StudentProfile profile) {
+        StudentProfile existing = repository.findById(id).orElse(null);
+
+        if (existing != null) {
+            existing.setEntrollmentId(profile.getEntrollmentId());
+            existing.setCohort(profile.getCohort());
+            existing.setYearLevel(profile.getYearLevel());
+            existing.setActive(profile.getActive());
+
+            return repository.save(existing);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteStudentProfile(Long id) {
+        repository.deleteById(id);
+    }
+}
