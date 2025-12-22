91547d0df763776a54d1ab4930fd6e2ff6797f1b
bala-amypo <bala@amypo.in>
2025-12-22 12:00:58 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -41,7 +41,7 @@ public class AuthController {
             return ResponseEntity.badRequest().body("Invalid credentials");
         }
         
-        String token = jwtUtil.generateToken(user.getUsername();
+        String token = jwtUtil.generateToken(user.getUsername());
         Map<String, String> response = new HashMap<>();
         response.put("token", token);
         
