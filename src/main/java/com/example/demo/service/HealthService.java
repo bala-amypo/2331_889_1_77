fd52500912d939c159903e33c7fe420d3d8fdc0d
bala-amypo <bala@amypo.in>
2025-12-29 04:45:26 +0000

    User commit

--- src/main/java/com/example/demo/service/HealthService.java
+++ src/main/java/com/example/demo/service/HealthService.java
@@ -1,11 +1,5 @@
 package com.example.demo.service;
 
-import org.springframework.stereotype.Service;
-
-@Service   // ⭐ FIRST AND MOST IMPORTANT CHANGE
-public class HealthService {
-
-    public String getStatus() {
-        return "OK";
-    }
+public interface HealthService {
+    String checkHealth();
 }
